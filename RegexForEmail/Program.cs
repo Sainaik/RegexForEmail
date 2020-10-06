2d072890b4a94bf454eb49fb2d995a8eb560e9e0
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-06 08:28:04 +0530

    [Sai]Reverted. Pattern to match Emails

--- RegexForEmail/Program.cs
+++ RegexForEmail/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegexForEmail
 {
@@ -7,6 +8,32 @@ namespace RegexForEmail
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!! Welcome to Pattern Matching Problem for Email");
+
+
+            //String Pattern = "^(abc.)[a-z0-9]*(@bridgelabz.co.)[a-z]+$";
+            // pattern = @"^([a-z][a-z0-9.+_-]*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?)$";
+            // [a-z]+[-+*.]?[0-9a-z]*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}
+
+            //String pattern = @"^[a-z]+[-+*.]?[0-9a-z]*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
+
+            String pattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$";
+
+            Regex r = new Regex(pattern);
+
+            Console.WriteLine("Enter the Email");
+
+            String email = Console.ReadLine();
+
+            if (r.IsMatch(email))
+            {
+                Console.WriteLine("Valid Email Address");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Email Address");
+
+            }
 
         }
     }
